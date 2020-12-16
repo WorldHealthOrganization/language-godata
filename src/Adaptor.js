4fa191ad902ebc712c190193b29ac16f1341f5b4
mamadou <mamadou.lakhassane@gmail.com>
2020-12-16 12:28:16 +0000

    get contact from an outbreak using filter, #2

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -107,22 +107,23 @@ export function listContacts(id, params, callback) {
 }
 
 /**
- * Get a specific contact from a query filter
+ * Get a specific contact withing an outbreak from a query filter
  * @public
  * @example
- *  getContact({"where":{"firstName": "Luca"}}, {}, state => {
+ *  getContact("343d-dc3e", {"where":{"firstName": "Luca"}}, {}, state => {
  *    console.log(state.data);
  *    return state;
  *  });
  * @function
+ * @param {string} id - Outbreak id
  * @param {object} query - An object with a query filter parameter
  * @param {object} params - Options, Headers parameters
  * @param {function} callback - (Optional) Callback function
  * @returns {Operation}
  */
-export function getContact(query, params, callback) {
+export function getContact(id, query, params, callback) {
   return state => {
-    const { host, id } = state.configuration;
+    const { host, access_token } = state.configuration;
 
     const { headers, body, options, ...rest } = expandReferences(params)(state);
 
@@ -130,11 +131,11 @@ export function getContact(query, params, callback) {
 
     return axios({
       baseURL: host,
-      url: '/users',
+      url: `/outbreaks/${id}/contacts`,
       method: 'GET',
       params: {
         filter,
-        access_token: id,
+        access_token,
       },
     })
       .then(response => {
